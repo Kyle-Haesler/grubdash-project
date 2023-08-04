9b179ce5924cd2429a9bc4826cee64ab90dedad6
Kyle Haesler <kyle.haesler@gmail.com>
2023-08-04 09:12:13 -0400

    Dishes list and post up and running

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,13 +6,47 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
-// validation middleware functions 
-
-
+// Validation Middleware Functions 
+// General Body Data Validation
+function bodyDataHas(propertyName){
+    return function (request, response, next){
+        const {data = {}} = request.body
+        if(data[propertyName]){
+            return next()
+        }
+        next({
+            status: 400,
+            message: `Dish must include a ${propertyName}`
+        })
+    }
+}
+// Price is valid
+function priceIsValid(request, response, next){
+    const {data: {price} = {}} = request.body
+    if(price <= 0 || !Number.isInteger(price)){
+        return next({
+            status: 400,
+            message: `Dish must have a price that is an integer greater than 0`
+        })
+    }
+    next()
+}
 // list function
 function list(request, response, next){
     response.json({data: dishes})
+}
+// create function
+function create(request, response, next){
+    const {data: {name, description, price, image_url} = {}} = request.body
+    const newDish = {
+        id: nextId(),
+        name,
+        description,
+        price,
+        image_url
+    }
+    dishes.push(newDish)
+    response.status(201).json({data: newDish})
 }
 
 
@@ -20,4 +54,12 @@ function list(request, response, next){
 
 module.exports = {
     list,
+    create: [
+        bodyDataHas("name"),
+        bodyDataHas("description"),
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        priceIsValid,
+        create
+    ]
 }
